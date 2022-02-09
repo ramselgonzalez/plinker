099cedc52130bf654378944f6581290895a772d7
Ramsel Gonzalez <ramsel.lgonzalez@gmail.com>
2022-02-08 16:15:22 -0800

    fix broken commit

--- pages/trials.tsx
+++ pages/trials.tsx
@@ -4,8 +4,9 @@ import DataRow from "components/DataRow";
 import MovePreview from "components/MovePreview";
 import MovePreviewHeader from "components/MovePreviewHeader";
 import MovePreviewContent from "components/MovePreviewContent";
-import MovePreviewData from "components/MovePreviewData";
 import MovePreviewImage from "components/MovePreviewImage";
+import StatSection from "components/StatSection";
+import StatSectionHeader from "components/StatSectionHeader";
 import Typography from "components/Typography";
 const Trials: NextPage = () => {
   return (
@@ -21,14 +22,14 @@ const Trials: NextPage = () => {
         </MovePreviewHeader>
         <MovePreviewContent className="trial-preview-content">
           <MovePreviewImage />
-          <MovePreviewData>
+          <StatSection>
             <DataRow label="Starter" value="Light" />
             <DataRow label="Damage" value="846,000" />
             <DataRow label="Position" value="Corner" />
             <DataRow label="Meter Gain" value="1.1" />
             <DataRow label="Difficulty" value="Easy" />
             <DataRow label="Assists" value="Solo" />
-          </MovePreviewData>
+          </StatSection>
         </MovePreviewContent>
       </MovePreview>
       <MovePreview to="/trial">
@@ -42,56 +43,14 @@ const Trials: NextPage = () => {
         </MovePreviewHeader>
         <MovePreviewContent className="trial-preview-content">
           <MovePreviewImage />
-          <MovePreviewData>
+          <StatSection>
             <DataRow label="Starter" value="Light" />
             <DataRow label="Damage" value="846,000" />
             <DataRow label="Position" value="Corner" />
             <DataRow label="Meter Gain" value="1.1" />
             <DataRow label="Difficulty" value="Easy" />
             <DataRow label="Assists" value="Solo" />
-          </MovePreviewData>
-        </MovePreviewContent>
-      </MovePreview>
-      <MovePreview to="/trial">
-        <MovePreviewHeader>
-          <Typography color="gray" gutter uppercase variant="subheading1">
-            Trial 3
-          </Typography>
-          <Typography shadow uppercase variant="h2">
-            Full Screen Level 3 Buster Confirm
-          </Typography>
-        </MovePreviewHeader>
-        <MovePreviewContent className="trial-preview-content">
-          <MovePreviewImage />
-          <MovePreviewData>
-            <DataRow label="Starter" value="Light" />
-            <DataRow label="Damage" value="846,000" />
-            <DataRow label="Position" value="Corner" />
-            <DataRow label="Meter Gain" value="1.1" />
-            <DataRow label="Difficulty" value="Easy" />
-            <DataRow label="Assists" value="Solo" />
-          </MovePreviewData>
-        </MovePreviewContent>
-      </MovePreview>
-      <MovePreview to="/trial">
-        <MovePreviewHeader>
-          <Typography color="gray" gutter uppercase variant="subheading1">
-            Trial 4
-          </Typography>
-          <Typography shadow uppercase variant="h2">
-            Full Screen Level 3 Buster Confirm
-          </Typography>
-        </MovePreviewHeader>
-        <MovePreviewContent className="trial-preview-content">
-          <MovePreviewImage />
-          <MovePreviewData>
-            <DataRow label="Starter" value="Light" />
-            <DataRow label="Damage" value="846,000" />
-            <DataRow label="Position" value="Corner" />
-            <DataRow label="Meter Gain" value="1.1" />
-            <DataRow label="Difficulty" value="Easy" />
-            <DataRow label="Assists" value="Solo" />
-          </MovePreviewData>
+          </StatSection>
         </MovePreviewContent>
       </MovePreview>
     </Container>
